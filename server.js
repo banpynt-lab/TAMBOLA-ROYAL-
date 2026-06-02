705c26b57ebac67668dc450190aa5bb1c5e4f4a1
banpynt-lab <banpynt@gmail.com>
2026-06-02 14:31:53 +0530

    final fix

--- server.js
+++ server.js
@@ -6,31 +6,22 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
+// Pynbeit: Wad static files na ka folder ba don u server.js (ym na public)
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
+  // Pynbeit: Wad index.html beit na root
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
-
-  socket.on("message", (data) => {
-    io.emit("message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+  socket.on("message", (data) => { io.emit("message", data); });
+  socket.on("disconnect", () => { console.log("User disconnected"); });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
